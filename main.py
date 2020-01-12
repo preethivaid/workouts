f2b9afaa8de1350fb20916e726c1dc07763f1ad3
Preethi Vaidyanathan <preethi@gaia-scope.com>
2020-01-12 13:12:20 -0500

    Use a csv

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 import os
 import time
+import pandas as pd
 from termcolor import colored
 
 interval_s = 1
-
-workouts = [
-    {'title': 'jumping jacks', 'description': 'blah'},
-    {'title': 'pushups', 'description': 'blah'},
-]
-
-for workout in workouts:
-    print(colored(workout['title'], 'green'), colored(workout['description'], 'magenta'))
+dir_path = os.path.dirname(os.path.realpath(__file__))
+workouts_df = pd.read_csv(os.path.join(dir_path, 'workouts.csv'))
+# os.system('say "Get ready to get your booty worked"')
+for _, row in workouts_df.iterrows():
+    print(colored(row.workout, 'green'), colored(row.description, 'magenta'))
     time.sleep(interval_s)
     os.system('say "Next"')
