6a6f293a8d29bb8f94668b1fed6dc81bc9caced9
Preethi Vaidyanathan <preethi@gaia-scope.com>
2020-01-12 13:25:03 -0500

    Add a progress bar

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ import os
 import time
 import pandas as pd
 from termcolor import colored
+from tqdm import tqdm
 
-interval_s = 1
+interval_s = 8
 dir_path = os.path.dirname(os.path.realpath(__file__))
 workouts_df = pd.read_csv(os.path.join(dir_path, 'workouts.csv'))
-# os.system('say "Get ready to get your booty worked"')
+
 for _, row in workouts_df.iterrows():
     print(colored(row.workout, 'green'), colored(row.description, 'magenta'))
-    time.sleep(interval_s)
+    for i in tqdm(range(interval_s)):
+        time.sleep(1)
     os.system('say "Next"')
